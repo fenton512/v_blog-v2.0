747f0c4b369e9b14f03541b2acf778469030c654
fenton <viktorken8264@gmail.com>
2025-10-02 23:33:40 +0300

    Create header without adaptive

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,36 +1,92 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <picture>
-  <source srcset="mdn-logo-wide.png" media="(min-width: 600px)" />
-  <img src="mdn-logo-narrow.png" alt="MDN" />
-</picture>
-  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
-   <!-- <TestPage/>
-   <router-view></router-view> -->
+  <div class="header">
+    <div class="logo">
+      <img src="./assets/logo_img.png" alt="" style="display: block; width: 100%; height: auto;">
+      <div class="logo-name">
+        VECTOR <br> BLOG
+      </div>
+    </div>
+    <nav class="nav-menu">
+      <a href="">Блог</a>
+      <a href="">Анонсы</a>
+      <a href="">О проекте</a>
+    </nav>
+    <div class="acc-panel">
+      <img src="./assets/avatar.svg" alt="" class="avatar">
+      <div class="acc-links">
+        <a href="">Регистрация</a>
+        |
+        <a href="">Вход</a>
+      </div>
+    </div>
+  </div>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-import TestPage from './components/TestPage.vue';
 
-
-@Options({
-  components: {
-    TestPage,
-    HelloWorld
-  },
+import { defineComponent} from 'vue';
+// import HelloWorld from './components/HelloWorld.vue';
+// import TestPage from './components/TestPage.vue';
+export default defineComponent({
+  name: "app",
+  components: {}
 })
-export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: var(--main-bg-color);
+  }
+  .header {
+    display: flex;
+    background-color: #682D66;
+    padding: 18px 16px;
+    height: 89px;
+    justify-content: space-between;
+  }
+  .logo {
+    width: 214px;
+    height: auto;
+    display: flex;
+    align-items: center;
+  }
+  .logo img {
+    width: auto;
+    height: auto;
+    max-width: 78px;
+    max-height: 85.22px;
+  }
+  .logo-name {
+    font-weight: 500;
+    font-size: 45px;
+
+    line-height: 67%;
+    letter-spacing: 0.02em;
+    text-transform: uppercase;
+    text-align: right;
+
+    padding-left: 2px;
+  }
+  .nav-menu {
+    width: 409px;
+    color: #fff;
+
+    font-weight: 500;
+    font-size: 45px;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 48px;
+  }
+  .acc-panel {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .acc-links {
+    font-weight: 500;
+    font-size: 45px;
+  }
 </style>

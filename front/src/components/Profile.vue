c32b29a42d641b5b3763c5d271cb96d2e6b7e6b7
Viktor Konovalov <viktor.konovalov.2006@mail.ru>
2026-04-15 13:24:24 +0300

    add new route and component for profile

---
+++ front/src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>PORNO</h1>
+    <div v-for="(key, value) in userStore.currentUser" :key="key">{{ value }}: {{ key }}</div>
+</template>
+
+
+<script lang="ts">
+import { useUserStore } from '@/stores/userStore';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    setup() {
+        const userStore = useUserStore()
+        return {userStore}
+    }
+})
+</script>
--- front/src/router.ts
+++ front/src/router.ts
@@ -2,11 +2,13 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import WelcomPage from "./components/WelcomPage.vue";
 import Auth from "./components/AuthChoice.vue";
+import Profile from "./components/Profile.vue"
 import AuthForm from "./components/AuthForm.vue"
 const routes = [
     {path: '/', component: WelcomPage},
     {path: '/auth', component: Auth},
-    {path: '/register', component:AuthForm}
+    {path: '/register', component:AuthForm},
+    {path: '/me', component: Profile}
     
 ]
 
